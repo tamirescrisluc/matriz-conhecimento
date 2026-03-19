01d1c3ee18a141ad3b3586deffeff9845c693b01
Tamires Cristina Silva Lucena <tamires.lucena@edata.info>
2026-03-19 12:22:54 -0300

    variaveis de ambiente supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://pnlxsfwpkdgqifeebcbu.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubHhzZndwa2RncWlmZWViY2J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MjQyOTgsImV4cCI6MjA4OTUwMDI5OH0.pTTjqCxnrZSmlbd3FYoa4EL87W-jBEnPUGb6_wBUFqw'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
